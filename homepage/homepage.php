da2e20add30c4cf513690ea0653a2cd083ed68ec
LauranceLi <138154118+LauranceLi@users.noreply.github.com>
2024-03-25 14:49:05 +0800

    將日曆改成中文

--- homepage/homepage.php
+++ homepage/homepage.php
@@ -39,8 +39,7 @@ if ($current_hour > $another_hour) {
     <div class="col-sm-12 col-md-6 col-xl-4">
       <div class="h-100 bg-secondary rounded p-4">
         <div class="d-flex align-items-center justify-content-between mb-4">
-          <h6 class="mb-0">Calender</h6>
-          <a href="">Show All</a>
+          <h6 class="mb-0">日曆</h6>
         </div>
         <div id="calender"></div>
       </div>
